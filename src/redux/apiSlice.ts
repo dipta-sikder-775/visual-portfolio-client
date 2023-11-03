08b6d0bd346eee1df8ab73d86217bb72ad7721e0
dipta-sikder-775 <anotheramit8@gmail.com>
2023-11-03 15:05:41 +0600

    - redux toolkit & rtk query added

---
+++ src/redux/apiSlice.ts
@@ -0,0 +1,8 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_APP_API_URL }),
+  endpoints: () => ({}),
+  tagTypes: ["getAllPhotos"],
+});
---
+++ src/redux/store.ts
@@ -0,0 +1,20 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "@redux/apiSlice";
+
+export const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(apiSlice.middleware),
+
+  devTools: import.meta.env.DEV,
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;

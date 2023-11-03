a687731ed5257b0286231ee30cc6316b6d9bc0ae
dipta-sikder-775 <anotheramit8@gmail.com>
2023-11-03 12:00:41 +0600

    - some initial settings changed to improve & optimize app

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths(), splitVendorChunkPlugin()],
+  build: {
+    cssMinify: true,
+  },
+});

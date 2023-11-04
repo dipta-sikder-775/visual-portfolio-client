a6c460c934d3c726c6431c63b6b0b7ee79274d07
dipta-sikder-775 <anotheramit8@gmail.com>
2023-11-04 14:28:42 +0600

    - layout modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Header from "@components/Header/Header";
 
 const App = () => {
   return (
     <div>
+      <Header />
       <div className="grid">
         {[1,2,3,4,5].map((id, index) => (
           <div key={index}>Hello</div>

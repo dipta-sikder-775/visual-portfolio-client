8378ea7128779573db0208703274fe8bac60adea
dipta-sikder-775 <anotheramit8@gmail.com>
2023-11-04 15:44:29 +0600

    - Dnd Kit added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
 import Header from "@components/Header/Header";
+import {
+  DndContext,
+  closestCenter,
+  DragOverlay,
+} from "@dnd-kit/core";
 
 const App = () => {
   return (
     <div>
       <Header />
       <div className="grid">
-        {[1,2,3,4,5].map((id, index) => (
-          <div key={index}>Hello</div>
-        ))}
+        <DndContext
+          sensors={sensors}
+          collisionDetection={closestCenter}
+          onDragStart={handleDragStart}
+          onDragEnd={handleDragEnd}
+          onDragCancel={handleDragCancel}
+        >
+          {/* Sortable context for rearranging items */}
+          <SortableContext items={items} strategy={rectSortingStrategy}>
+            {content}
+          </SortableContext>
+
+          {/* Drag overlay for the active dragging photo */}
+          <DragOverlay adjustScale={true}>
+            {activeId && (
+              <Photo
+                url={getAllPhotosRes?.[activeId]?.url ?? ""}
+                index={items.indexOf(activeId)}
+                isDragging
+                photoId={activeId}
+              />
+            )}
+          </DragOverlay>
+        </DndContext>
       </div>
     </div>
   );

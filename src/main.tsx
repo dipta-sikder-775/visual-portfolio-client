0529203b364a4d64cc2ada2e5cf9f0ecd9a3a07f
dipta-sikder-775 <anotheramit8@gmail.com>
2023-11-05 23:33:24 +0600

    - comments added for better comde understanding git commit -m

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "@redux/store.ts";
+import { Toaster } from "react-hot-toast";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+      <Toaster />
+    </Provider>
+  </React.StrictMode>
+);
